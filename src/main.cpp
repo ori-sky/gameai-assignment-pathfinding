77c3e78d957e52063ff7cb4ca8ec719055f4304f
Shockk <shokku.ra@gmail.com>
2016-03-05 09:48:54 +0000

    main: build map of vertex names for lookup

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <boost/unordered_map.hpp>
 #include <boost/graph/graphviz.hpp>
 
 struct vertex_pos_t {
@@ -9,7 +10,7 @@ struct edge_label_t {
 };
 
 typedef boost::property<vertex_pos_t, std::string> vertex_pos_p;
-typedef boost::property<boost::vertex_name_t, std::string, vertex_pos_p> vertex_p;
+typedef boost::property<boost::vertex_name_t, int, vertex_pos_p> vertex_p;
 typedef boost::property<boost::edge_weight_t, int> edge_p;
 typedef boost::no_property graph_p;
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, vertex_p, edge_p, graph_p> graph_t;
@@ -25,7 +26,18 @@ int main(int argc, char **argv) {
 
 	boost::read_graphviz(in, g, dp);
 
+	typedef boost::graph_traits<graph_t>::vertex_descriptor vertex_t;
+	boost::unordered_map<int, vertex_t> vertex_map;
+	BGL_FORALL_VERTICES(v, g, graph_t) {
+		vertex_map.emplace(boost::get(boost::vertex_name, g, v), v);
+	}
+
+	auto &v1  = vertex_map[1];
+	auto &v61 = vertex_map[61];
+
 	boost::write_graphviz_dp(std::cout, g, dp);
+	std::cout << "    " << boost::get(vertex_pos_t(), g, v1) << std::endl;
+	std::cout << "    " << boost::get(vertex_pos_t(), g, v61) << std::endl;
 
 	return 0;
 }
