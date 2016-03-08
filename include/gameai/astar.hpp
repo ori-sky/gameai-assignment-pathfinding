75756788e34283ec7d85a83aad7ea3373e538983
Shockk <shokku.ra@gmail.com>
2016-03-08 10:36:55 +0000

    astar: rework to simplify use

--- include/gameai/astar.hpp
+++ include/gameai/astar.hpp
@@ -3,59 +3,73 @@
 #include <gameai/astar_vertex.hpp>
 
 namespace gameai {
-	/* generalization can sometimes be ugly */
-	template<typename HeuristicF, typename VertexPosT, typename GraphT>
-	void astar(const typename boost::graph_traits<GraphT>::vertices_size_type &src,
-	           const typename boost::graph_traits<GraphT>::vertices_size_type &dst,
-	           const GraphT &g) {
-		typedef typename boost::graph_traits<GraphT>::vertex_descriptor VertexT;
-		boost::unordered_map<typename boost::graph_traits<GraphT>::vertices_size_type, VertexT> vertex_map;
+	struct astar_pos_t { typedef boost::vertex_property_tag kind; };
 
-		BGL_FORALL_VERTICES_T(v, g, GraphT) {
-			vertex_map.emplace(boost::get(boost::vertex_name, g, v), v);
-		}
+	template<typename HeuristicF, typename GraphT> class astar {
+	public:
+		typedef GraphT graph_type;
+		typedef typename boost::graph_traits<graph_type> graph_traits_type;
+		typedef typename boost::graph_traits<graph_type>::vertex_descriptor vertex_type;
+		typedef astar_vertex<vertex_type> astar_vertex_type;
+	private:
+		HeuristicF heuristic;
+		const graph_type &g;
+	public:
+		astar(const graph_type &g) : g(g) {}
 
-		/* a heap-based priority queue is used to retrive the lowest-cost open node
-		 * hash-based sets are used to lookup nodes in O(1)
-		 */
-		boost::heap::priority_queue<astar_vertex<VertexT>, boost::heap::compare<inverse_comparator<astar_vertex<VertexT>>>> open_heap;
-		boost::unordered_set<astar_vertex<VertexT>> open_set;
-		boost::unordered_set<astar_vertex<VertexT>> closed_set;
+		void path(const typename graph_traits_type::vertices_size_type &src,
+				  const typename graph_traits_type::vertices_size_type &dst) {
+			boost::unordered_map<typename graph_traits_type::vertices_size_type, vertex_type> vertex_map;
 
-		astar_vertex<VertexT> v{0, 0, src, vertex_map.at(src)};
-		open_heap.emplace(v);
-		open_set.emplace(v);
-
-		while(!open_heap.empty()) {
-			auto current = open_heap.top();
-			open_heap.pop();
-			open_set.erase(current);
-
-			std::cout << boost::get(boost::vertex_name, g, current.vertex) << std::endl;
-
-			BOOST_FOREACH(auto edge, boost::out_edges(current.vertex, g)) {
-				auto target = boost::target(edge, g);
-				auto n = boost::get(boost::vertex_name, g, target);
-				if(n == dst) {
-					std::cout << dst << std::endl;
-					return;
-				}
-
-				auto w = boost::get(boost::edge_weight, g, edge);
-				auto dist_to_end  = HeuristicF()(boost::get(VertexPosT(), g, target),
-				                                 boost::get(VertexPosT(), g, vertex_map.at(dst)));
-				astar_vertex<VertexT> next{current.base_cost + w, dist_to_end, n, target};
-
-				auto open_find = open_set.find(next);
-				if(open_find != open_set.end() && open_find->total_cost() < next.total_cost()) { continue; }
-
-				auto closed_find = closed_set.find(next);
-				if(closed_find != closed_set.end() && closed_find->total_cost() < next.total_cost()) { continue; }
-
-				open_heap.emplace(next);
-				open_set.emplace(next);
+			BGL_FORALL_VERTICES_T(v, g, graph_type) {
+				vertex_map.emplace(boost::get(boost::vertex_name, g, v), v);
 			}
-			closed_set.emplace(current);
+
+			/* a heap-based priority queue is used to retrive the lowest-cost open node
+			 * hash-based sets are used to lookup nodes in O(1)
+			 */
+			boost::heap::priority_queue<astar_vertex_type, boost::heap::compare<inverse_comparator<astar_vertex_type>>> open_heap;
+			boost::unordered_set<astar_vertex_type> open_set;
+			boost::unordered_set<astar_vertex_type> closed_set;
+
+			astar_vertex_type v{0, 0, src, vertex_map.at(src)};
+			open_heap.emplace(v);
+			open_set.emplace(v);
+
+			while(!open_heap.empty()) {
+				auto current = open_heap.top();
+				open_heap.pop();
+				open_set.erase(current);
+
+				std::cout << boost::get(boost::vertex_name, g, current.vertex) << std::endl;
+
+				BOOST_FOREACH(auto edge, boost::out_edges(current.vertex, g)) {
+					auto target = boost::target(edge, g);
+					auto n = boost::get(boost::vertex_name, g, target);
+					if(n == dst) {
+						std::cout << dst << std::endl;
+						return;
+					}
+
+					auto w = boost::get(boost::edge_weight, g, edge);
+					auto dist_to_end = heuristic(boost::get(astar_pos_t(), g, target),
+					                             boost::get(astar_pos_t(), g, vertex_map.at(dst)));
+					astar_vertex_type next{current.base_cost + w, dist_to_end, n, target};
+
+					auto open_find = open_set.find(next);
+					if(open_find != open_set.end() && open_find->total_cost() < next.total_cost()) { continue; }
+
+					auto closed_find = closed_set.find(next);
+					if(closed_find != closed_set.end() && closed_find->total_cost() < next.total_cost()) { continue; }
+
+					open_heap.emplace(next);
+					open_set.emplace(next);
+				}
+				closed_set.emplace(current);
+			}
 		}
+	};
+	template<typename HeuristicF, typename GraphT> astar<HeuristicF, GraphT> make_astar(const GraphT &g) {
+		return astar<HeuristicF, GraphT>(g);
 	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@
 #include <gameai/point2.hpp>
 #include <gameai/astar.hpp>
 
-struct vertex_pos_t { typedef boost::vertex_property_tag kind; };
 struct edge_label_t { typedef boost::edge_property_tag kind; };
 
-typedef boost::property<vertex_pos_t, gameai::point2<int>> vertex_pos_p;
+typedef boost::property<gameai::astar_pos_t, gameai::point2<int>> vertex_pos_p;
 typedef boost::property<boost::vertex_name_t, boost::graph_traits<boost::adjacency_list<>>::vertices_size_type, vertex_pos_p> vertex_p;
 typedef boost::property<boost::edge_weight_t, unsigned int> edge_p;
 typedef boost::no_property graph_p;
@@ -19,12 +18,14 @@ int main(int argc, char **argv) {
 
 	graph_t g;
 	boost::dynamic_properties dp(boost::ignore_other_properties);
-	dp.property("node_id", boost::get(boost::vertex_name, g));
-	dp.property("pos",     boost::get(vertex_pos_t(),     g));
-	dp.property("label",   boost::get(boost::edge_weight, g));
+	dp.property("node_id", boost::get(boost::vertex_name,    g));
+	dp.property("pos",     boost::get(gameai::astar_pos_t(), g));
+	dp.property("label",   boost::get(boost::edge_weight,    g));
 
 	boost::read_graphviz(in, g, dp);
-	gameai::astar<gameai::square_distance, vertex_pos_t>(1, 61, g);
+
+	auto astar = gameai::make_astar<gameai::square_distance>(g);
+	astar.path(1, 61);
 
 	return 0;
 }
